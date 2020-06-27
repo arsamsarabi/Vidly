31d1afe2054b11fce324bf959d79931abbfd43b2
Arsam Sarabi <arsamsarabi@me.com>
2020-06-27 10:37:31 +0100

    ✨ simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello Worlds!')
+import express from 'express'
+
+const app = express()
+const { PORT = 4200 }: { PORT?: number } = process.env
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({ Hello: 'World!' }))
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
