e33edd8c1ee9f60a1e0d79cd8b8451105f2ca466
Arsam Sarabi <arsamsarabi@me.com>
2020-07-04 13:18:01 +0100

    ✨ customer api exposed

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
+import customer from './customer'
 import genre from './genre'
 import movie from './movie'
 
 const router = express.Router()
 
+router.use('/customer', customer)
 router.use('/genre', genre)
 router.use('/movie', movie)
 
