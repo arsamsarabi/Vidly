3660af90d64a3d71c43880ac8793240afe5ef417
Arsam Sarabi <arsamsarabi@me.com>
2020-07-05 11:24:05 +0100

    ✨ reantal route exposed

--- app/api/index.ts
+++ app/api/index.ts
@@ -3,11 +3,13 @@ import express from 'express'
 import customer from './customer'
 import genre from './genre'
 import movie from './movie'
+import rental from './rental'
 
 const router = express.Router()
 
 router.use('/customer', customer)
 router.use('/genre', genre)
 router.use('/movie', movie)
+router.use('/rental', rental)
 
 export default router
